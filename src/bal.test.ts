2c3cdf0291e659bb9296a2909c86fa44a65150fc
Entreprenerd <alex@entreprenerd.xyz>
2023-05-23 09:51:16 +0200

    fix: bal tests (with 8 digits of rounding)

--- src/bal.test.ts
+++ src/bal.test.ts
@@ -1,5 +1,7 @@
 import { makeAmountOutFunction } from "./make";
 
+const comparable = (val) => Math.floor(val / 1e8);
+
 /**
  * 
   Creating wstETH WETH Pool
@@ -26,14 +28,18 @@ import { makeAmountOutFunction } from "./make";
 
   uint256 WETH_BAL = 1063322810377902132666;
   uint8 DECIMALS = 18;
+
+  https://optimistic.etherscan.io/address/0xBA12222222228d8Ba445958a75a0704d566BF2C8#code
+  0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb200020000000000000000008b
+  https://optimistic.etherscan.io/address/0x9aa3cd420f830E049e2b223D0b07D8c809C94d15
  * 
  */
 
-describe("Balancer Stable", () => {
-  const fromValToRate = (val) => (val * 1124504367992424664) / 1e18;
+describe("Balancer Stable, 8 digits of imprecision", () => {
+  const fromValToRate = (val) => (val * 1125263028751930526) / 1e18;
   const reserves = [
-    fromValToRate(1063322810377902132666),
-    1063322810377902132666,
+    fromValToRate(1103816941445067400656),
+    1020049918708824070182,
   ];
 
   const getAmountOut = makeAmountOutFunction("Balancer", reserves, true);
@@ -42,21 +48,21 @@ describe("Balancer Stable", () => {
   // TODO: Fix Math
   // Prob pool settings or something
 
-  describe("Balancer USDC WETH", () => {
+  describe("Balancer wstETH WETH", () => {
     it("Velo 1", () => {
-      expect(getAmountOut(fromValToRate(1000000000000000000))).toBe(
-        1124945242199231826
+      expect(comparable(getAmountOut(fromValToRate(1000000000000000000)))).toBe(
+        comparable(1124698774312831587)
       );
     });
     it("Velo 2", () => {
-      expect(getAmountOut(fromValToRate(10000000000000000000))).toBe(
-        11249251086187577318
-      );
+      expect(
+        comparable(getAmountOut(fromValToRate(10000000000000000000)))
+      ).toBe(comparable(11246774022270881393));
     });
     it("Velo 3", () => {
-      expect(getAmountOut(fromValToRate(50000000000000000000))).toBe(
-        56241757344862105392
-      );
+      expect(
+        comparable(getAmountOut(fromValToRate(50000000000000000000)))
+      ).toBe(comparable(56229016387923591565));
     });
   });
 });
